03079bce8560e23c39f24fedbf1b57be438f4bea
Insaniity <iiyolo@icloud.com>
2018-01-22 20:33:41 +0000

    aaa
    aaaa

--- src/Commands.js
+++ src/Commands.js
@@ -1,9 +1,10 @@
 module.exports = (message, config, Discord, fs, latestVersion) => {
-	var blockCommand, removeBlockCommand, banCommand, clearCommand, verifylogs, versionCommand, createRole;
+	var blockCommand, removeBlockCommand, banCommand, clearCommand, verifylogs, versionCommand, createRole, pingCommand;
 	config["commands"]["blockUser"].enabled ? blockCommand = require("../commands/block.js") : blockCommand = false;
 	config["commands"]["removeBlockFromUser"].enabled ? removeBlockCommand = require("../commands/removeBlock.js") : removeBlockCommand = false;
 	config["commands"]["banGuildMember"].enabled ? banCommand = require("../commands/removeBlock.js") : banCommand = false;
 	config["commands"]["clear"].enabled ? clearCommand = require("../commands/clear.js") : clearCommand = false;
+	config["commands"]["ping"].enabled ? pingCommand = true : pingCommand = false;
 	config["commands"]["version"].enabled ? versionCommand = true : versionCommand = false;
 	config["commands"]["makerole"].enabled ? createRole = require("../commands/makerole.js") : createRole = false;
 	config.logging ? verifylogs = require("../src/logs.json") : verifylogs = false;
@@ -27,6 +28,10 @@ module.exports = (message, config, Discord, fs, latestVersion) => {
 		case config.prefix + config["commands"]["clear"].command:
 			if (!clearCommand) return;
 			clearCommand(message, config["commands"]["clear"].contributors);
+			break;
+		case config.prefix + config["commands"]["ping"].command:
+			if (!pingCommand) return;
+			message.channel.send("pong?");
 			break;
 		case config.prefix + "eval":
 			if (message.author.id === config.ownerid && config.evalAllowed === "true") {
